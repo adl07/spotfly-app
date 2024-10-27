3443a388de2461192f767fc4f936af5359a2067f
Adriel <adrielalegre721@gmail.com>
2024-10-27 17:06:11 -0300

    04-creacion de source control + listado de canciones

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,7 +2,7 @@ import {Pause, Play} from './Player'
 
 import { usePlayerStore } from '@/store/playerStore'
 
-export function CardPlayButton({id}) {
+export function CardPlayButton({id, size= 'small'}) {
 
 const {currentMusic, isPlaying, setIsPlaying, setCurrentMusic} = usePlayerStore(state => state)
 
@@ -25,10 +25,12 @@ const handleClick=()=>{
     })
 }
 
+const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
 
 return (
-    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4'>
-        {isPlayingPlayList ? <Pause/> : <Play/>}
+    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-4 mt-4 
+    hover:scale-105 transition hover:bg-green-400'>
+        {isPlayingPlayList ? <Pause className={iconClassName}/> : <Play className={iconClassName}/>}
     </button>
   )
 }
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -1,8 +1,13 @@
 import { create} from "zustand";
 
+//Estados Globales//
+
 export const usePlayerStore = create((set)=>({
     isPlaying: false,
     currentMusic: {playlist: null, song: null, songs:[]},
+    volume: 1,
+    setVolume: (volume) => set({volume}),
     setIsPlaying: (isPlaying)=> set({isPlaying}),
     setCurrentMusic: (currentMusic) => set({currentMusic}),
+
 }))
